0d3155cf1180a1fdf42bfc697317a825fc20adae
Thiago Barros <bunbismuth@proton.me>
2023-11-15 08:12:57 -0300

    feat: added PedidosController class

---
+++ src/main/java/com/bunbismuth/springfilter/controller/PedidosController.java
@@ -0,0 +1,24 @@
+package com.bunbismuth.springfilter.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PedidosController {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PedidosController.class);
+
+  @RequestMapping("/pedidos")
+  public String getPedido() {
+    LOGGER.info("Pedido 123 cadastrado");
+    return "Seu pedido foi cadastrado!";
+  }
+
+  @RequestMapping("/usuarios")
+  public String getUser() {
+    LOGGER.info("Usuario Joao do Arrasta cadastrado");
+    return "Bem-vindo Joao do Arrasta";
+  }
+}
